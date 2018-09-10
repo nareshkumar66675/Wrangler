1f6f37f2cd8818be382dd3c507474bc950ff7cf4
Naresh Kumar <nareshkumar66675@gmail.com>
2018-09-10 06:43:27 -0500

    Data Files and Code

---
+++ src/Titanic.py
@@ -0,0 +1,13 @@
+from pandas import DataFrame, read_csv
+
+# General syntax to import a library but no functions: 
+##import (library) as (give the library a nickname/alias)
+import matplotlib.pyplot as plt
+import pandas as pd #this is how I usually import pandas
+import sys #only needed to determine Python version number
+import matplotlib #only needed to determine Matplotlib version number
+
+# Enable inline plotting
+# %matplotlib inline
+
+print(3+4)
